62efc8b3b216504e5a0fbba72be2e23e91355d43
BRUNOBONIFACEKAFYULILO <126581210+BRUNOBONIFACEKAFYULILO@users.noreply.github.com>
2023-03-21 10:53:47 -0800

    Add files via upload

---
+++ overtime.cpp
@@ -0,0 +1,19 @@
+//BRUNOO BONIFACE KAFYULILO
+#include<iostream>
+using namespace std;
+int main()
+{
+	int hour,wages,normal,overtime;
+	cin>>hour>>wages;
+//		if(hour>30)
+//	{
+//		overtime= 2* 30 * wages;
+//		cout<<"overtime is"<<overtime<<endl;
+//		return 0;
+//	}
+	hour>30?overtime=2*30*wages:return 0;
+	normal=hour*wages;
+cout<<"normal is"<<normal<<endl;
+return 0;
+}
+
